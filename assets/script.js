5644a3ea3b20e923f3774fb7678fc1321176a38b
Martin Homola <adder10@gmail.com>
2018-05-11 09:46:24 +0200

    prettify js, html, css

--- assets/script.js
+++ assets/script.js
@@ -1,309 +1,310 @@
-(function() {
-	// setting
+( function() {
+  // setting
 
-	var app = {
-		// how many days from today
-		days: 7,
-		// currencies sorted by market capital,
-		cryptocurrenciesAmount: 10,
-		// to which prices you want to transfer
-		fiat: 'USD',
-		// day to
-		dateTo: null,
-		// day to
-		dateFrom: null,
-		// preloader interval
-		preloaderInterval: null,
-
-
-		/********************
-		* get prices historical data for one crypto currency
-		*
-		* @parameter - crypto currency
-		* @return - ajax function
-		*/
-		single: function (crypto) {
-			var symbol = crypto.symbol,
-				fiat = app.fiat;
-
-			// exceptions
-			if(crypto.symbol === 'MIOTA') {
-				symbol = 'IOT';
-			}
-
-			// if fiat is BTC & crypto is BTC - api will response
-			// with error message
-			if(app.fiat === 'BTC' && crypto.symbol === 'BTC') {
-				fiat = 'USD';
-			}
-
-			// default given data
-			var data = {
-				limit: app.days, // amount of days
-				fiat: fiat,
-				cryptoCurrency: symbol,
-			};
-
-			if(app.dateTo) {
-				data.timestamp = app.dateTo / 1000; // to seconds
-			}
-
-			return $.ajax({
-				url: './data-currency',
-				method: 'POST',
-				dataType: 'json',
-				data: data
-			});
-		},
-
-		/********************
-		* save data to the file
-		*
-		* @parameter - format (csv, xml, json), data
-		* 
-		*/
-		saveFile: function(data) {
-			$.ajax({
-				url: './save',
-				method: 'POST',
-				dataType: 'json',
-				data: {
-					data: JSON.stringify(data)
-				},
-				success: function(data) {
-					$('.preloader').remove();
-					clearInterval(preloaderInterval);
-
-					if(!$('.download-buttons').length > 0) {
-						$('#app').append(
-							'<div class="button-wrap download-buttons">' +
-								'<a class="button" href="./data/data.json" download>Download .json</a><br />' +
-								'<a class="button" href="./data/data.csv" download>Download .csv</a><br />' +
-								'<a class="button js-table-open" href="#">Just show data</a>' +
-							'</div>'
-						);
-					}
-
-					// add open event
-					$('.js-table-open').click(function() {
-						$('.table-wrap').addClass('active');
-					});
-
-					// add close event
-					$('.js-table-close').click(function() {
-						$('.table-wrap').removeClass('active');
-					});
-				}
-			});
-		},
-
-		// preloader
-		addPreloader: function() {
-			$('body').append(
-				'<div class="preloader">' +
-					'<div class="rocket-trajectory">' +
-						'<div class="rocket">' +
-							'<i class="rocket__fuel rocket__fuel--1"></i>' +
-							'<i class="rocket__fuel rocket__fuel--2"></i>' +
-						'</div>' +
-					'</div>' +
-					'<div class="preloader__loading">Loading data<span class="js-dot"></span></div>' +
-				'</div>'
-			);
-
-			// animate loading dots
-			preloaderInterval = setInterval(function() {
-				var $dots = $('.js-dot');
-					dotsLen = $dots.text().length;
-				if(dotsLen < 3) {
-					$dots.text($dots.text() + '.');
-				} else {
-					$dots.text('');
-				}	
-			}, 500);
-		},
-
-		addHtmlTable: function(data) {
-			var $dataRows = [];
-
-			for(var i = 0; i < data.length; i++) {
-				for(var j = 0; j < data[i].data.length; j++) {
-					var date = new Date(data[i].data[j].time * 1000);
-					$dataRows.push($(
-						'<tr>' +
-							'<td>' + date.getDate() + '. ' + (date.getMonth() + 1) + '. ' + date.getFullYear() + '</td>' +
-							'<td>' + data[i].name + '</td>' +
-							'<td>' + data[i].data[j].close + '</td>' +
-							'<td>' + data[i].data[j].high + '</td>' +
-							'<td>' + data[i].data[j].low + '</td>' +
-							'<td>' + data[i].data[j].open + '</td>' +
-							'<td>' + data[i].data[j].volumefrom + '</td>' +
-							'<td>' + data[i].data[j].volumeto + '</td>' +
-						'</tr>'
-					));
-				}
-			}
-
-			// create table if not exist else remove data from body
-			if(!$('.js-table')[0].childNodes.length > 0) {
-				$('.js-table').append(
-					'<table class="u-full-width">' +
-						'<thead>' +
-							'<tr>' +
-								'<th>Date</th>' +
-								'<th>Coin</th>' +
-								'<th>Close</th>' +
-								'<th>High</th>' +
-								'<th>Low</th>' +
-								'<th>Open</th>' +
-								'<th>Volume from</th>' +
-								'<th>Volume to</th>' +
-							'</tr>' +
-						'</thead>' +
-						'<tbody></tbody>' +
-					'</table>'
-				);
-			} else {
-				$('tbody > tr').remove();
-			}
-
-			for(var i = 0; i < $dataRows.length; i++) {
-				$('tbody').append($dataRows[i]);
-			}
-		},
-
-		/********************
-		* 1. get all currencies from ajax call
-		* 2. create request on all gotten currencies
-		* 3. save data into the currencies object
-		* 4. process data
-		*
-		*/
-		init: function() {
-			app.addPreloader();
-
-			$.ajax({
-				url: './data-all-currencies',
-				method: 'POST',
-				dataType: 'json',
-				data: {
-					amount: app.cryptocurrenciesAmount
-				},
-				success: function(response) {
-					var requests = [],
-						data = response.data;
-						dataKeys = Object.keys(data),
-						dataArray = [];
-
-					// sorted keys
-					dataKeys.sort(function(a, b) {
-						return data[a].rank - data[b].rank;
-					});
-
-					for(var i = 0; i < dataKeys.length; i++) {
-						dataArray.push(data[dataKeys[i]]); // push to array item by rank
-						requests.push(app.single(dataArray[i])); // create requests
-					}
-
-					// aysnchronous operation - waiting for all requests will be done
-					$.when.apply(null, requests).then(function() {
-
-						for(var i = 0; i < arguments.length; i++) {
-							dataArray[i].data = arguments[i][0].Data;
-
-							// if fiat is BTC & crypto is BTC - api will response
-							// with error message
-							if(app.fiat === 'BTC' && dataArray[i].symbol === 'BTC') {
-								for(var j = 0; j < dataArray[i].data.length; j++) {
-									dataArray[i].data[j].close = 1;
-									dataArray[i].data[j].high = 1;
-									dataArray[i].data[j].low = 1;
-									dataArray[i].data[j].open = 1;								
-									dataArray[i].data[j].volumefrom = 1;
-									dataArray[i].data[j].volumeto = 1;
-								}
-							}
-						}
-
-						app.addHtmlTable(dataArray);
-						app.saveFile(dataArray);
-					});
-				}
-			});
-		}
-	};
+  var app = {
+    // how many days from today
+    days: 7,
+    // currencies sorted by market capital,
+    cryptocurrenciesAmount: 10,
+    // to which prices you want to transfer
+    fiat: 'USD',
+    // day to
+    dateTo: null,
+    // day to
+    dateFrom: null,
+    // preloader interval
+    preloaderInterval: null,
 
 
-	// TODO: more code readable
-	// on window ready set dates
-	var today = new Date();
-		beforeWeek = new Date();
+    /********************
+     * get prices historical data for one crypto currency
+     *
+     * @parameter - crypto currency
+     * @return - ajax function
+     */
+    single: function( crypto ) {
+      var symbol = crypto.symbol,
+        fiat = app.fiat;
 
-	beforeWeek.setDate(today.getDate() - 6);
+      // exceptions
+      if ( crypto.symbol === 'MIOTA' ) {
+        symbol = 'IOT';
+      }
 
-	function formatDateForInput(date) {
-		var years = date.getFullYear(),
-			months = date.getMonth() + 1,
-			days = date.getDate();
+      // if fiat is BTC & crypto is BTC - api will response
+      // with error message
+      if ( app.fiat === 'BTC' && crypto.symbol === 'BTC' ) {
+        fiat = 'USD';
+      }
 
-		if(months < 10) {
-			months = '0' + months;
-		}
+      // default given data
+      var data = {
+        limit: app.days, // amount of days
+        fiat: fiat,
+        cryptoCurrency: symbol,
+      };
 
-		if(days < 10) {
-			days = '0' + days;
-		}
+      if ( app.dateTo ) {
+        data.timestamp = app.dateTo / 1000; // to seconds
+      }
 
-		return years + '-' + months + '-' + days;
-	}
+      return $.ajax( {
+        url: './data-currency',
+        method: 'POST',
+        dataType: 'json',
+        data: data
+      } );
+    },
 
-	var _MS_PER_DAY = 1000 * 60 * 60 * 24;
-	// a and b are javascript Date objects
-	function dateDiffInDays(a, b) {
-		// Discard the time and time-zone information.
-		var utc1 = Date.UTC(a.getFullYear(), a.getMonth(), a.getDate());
-		var utc2 = Date.UTC(b.getFullYear(), b.getMonth(), b.getDate());
+    /********************
+     * save data to the file
+     *
+     * @parameter - format (csv, xml, json), data
+     * 
+     */
+    saveFile: function( data ) {
+      $.ajax( {
+        url: './save',
+        method: 'POST',
+        dataType: 'json',
+        data: {
+          data: JSON.stringify( data )
+        },
+        success: function( data ) {
+          $( '.preloader' ).remove();
+          clearInterval( preloaderInterval );
 
-		return Math.floor((utc2 - utc1) / _MS_PER_DAY);
-	}
+          if ( !$( '.download-buttons' ).length > 0 ) {
+            $( '#app' ).append(
+              '<div class="button-wrap download-buttons">' +
+              '<a class="button" href="./data/data.json" download>Download .json</a><br />' +
+              '<a class="button" href="./data/data.csv" download>Download .csv</a><br />' +
+              '<a class="button js-table-open" href="#">Just show data</a>' +
+              '</div>'
+            );
+          }
 
-	$('#date-to').val(formatDateForInput(today));
-	$('#date-from').val(formatDateForInput(beforeWeek));
+          // add open event
+          $( '.js-table-open' ).click( function() {
+            $( '.table-wrap' ).addClass( 'active' );
+          } );
 
-	app.days = dateDiffInDays(beforeWeek, today);
+          // add close event
+          $( '.js-table-close' ).click( function() {
+            $( '.table-wrap' ).removeClass( 'active' );
+          } );
+        }
+      } );
+    },
+
+    // preloader
+    addPreloader: function() {
+      $( 'body' ).append(
+        '<div class="preloader">' +
+        '<div class="rocket-trajectory">' +
+        '<div class="rocket">' +
+        '<i class="rocket__fuel rocket__fuel--1"></i>' +
+        '<i class="rocket__fuel rocket__fuel--2"></i>' +
+        '</div>' +
+        '</div>' +
+        '<div class="preloader__loading">Loading data<span class="js-dot"></span></div>' +
+        '</div>'
+      );
+
+      // animate loading dots
+      preloaderInterval = setInterval( function() {
+        var $dots = $( '.js-dot' );
+        dotsLen = $dots.text().length;
+        if ( dotsLen < 3 ) {
+          $dots.text( $dots.text() + '.' );
+        } else {
+          $dots.text( '' );
+        }
+      }, 500 );
+    },
+
+    addHtmlTable: function( data ) {
+      var $dataRows = [];
+
+      for ( var i = 0; i < data.length; i++ ) {
+        for ( var j = 0; j < data[ i ].data.length; j++ ) {
+          var date = new Date( data[ i ].data[ j ].time * 1000 );
+          $dataRows.push( $(
+            '<tr>' +
+            '<td>' + date.getDate() + '. ' + ( date.getMonth() + 1 ) + '. ' + date.getFullYear() +
+            '</td>' +
+            '<td>' + data[ i ].name + '</td>' +
+            '<td>' + data[ i ].data[ j ].close + '</td>' +
+            '<td>' + data[ i ].data[ j ].high + '</td>' +
+            '<td>' + data[ i ].data[ j ].low + '</td>' +
+            '<td>' + data[ i ].data[ j ].open + '</td>' +
+            '<td>' + data[ i ].data[ j ].volumefrom + '</td>' +
+            '<td>' + data[ i ].data[ j ].volumeto + '</td>' +
+            '</tr>'
+          ) );
+        }
+      }
+
+      // create table if not exist else remove data from body
+      if ( !$( '.js-table' )[ 0 ].childNodes.length > 0 ) {
+        $( '.js-table' ).append(
+          '<table class="u-full-width">' +
+          '<thead>' +
+          '<tr>' +
+          '<th>Date</th>' +
+          '<th>Coin</th>' +
+          '<th>Close</th>' +
+          '<th>High</th>' +
+          '<th>Low</th>' +
+          '<th>Open</th>' +
+          '<th>Volume from</th>' +
+          '<th>Volume to</th>' +
+          '</tr>' +
+          '</thead>' +
+          '<tbody></tbody>' +
+          '</table>'
+        );
+      } else {
+        $( 'tbody > tr' ).remove();
+      }
+
+      for ( var i = 0; i < $dataRows.length; i++ ) {
+        $( 'tbody' ).append( $dataRows[ i ] );
+      }
+    },
+
+    /********************
+     * 1. get all currencies from ajax call
+     * 2. create request on all gotten currencies
+     * 3. save data into the currencies object
+     * 4. process data
+     *
+     */
+    init: function() {
+      app.addPreloader();
+
+      $.ajax( {
+        url: './data-all-currencies',
+        method: 'POST',
+        dataType: 'json',
+        data: {
+          amount: app.cryptocurrenciesAmount
+        },
+        success: function( response ) {
+          var requests = [],
+            data = response.data;
+          dataKeys = Object.keys( data ),
+            dataArray = [];
+
+          // sorted keys
+          dataKeys.sort( function( a, b ) {
+            return data[ a ].rank - data[ b ].rank;
+          } );
+
+          for ( var i = 0; i < dataKeys.length; i++ ) {
+            dataArray.push( data[ dataKeys[ i ] ] ); // push to array item by rank
+            requests.push( app.single( dataArray[ i ] ) ); // create requests
+          }
+
+          // aysnchronous operation - waiting for all requests will be done
+          $.when.apply( null, requests ).then( function() {
+
+            for ( var i = 0; i < arguments.length; i++ ) {
+              dataArray[ i ].data = arguments[ i ][ 0 ].Data;
+
+              // if fiat is BTC & crypto is BTC - api will response
+              // with error message
+              if ( app.fiat === 'BTC' && dataArray[ i ].symbol === 'BTC' ) {
+                for ( var j = 0; j < dataArray[ i ].data.length; j++ ) {
+                  dataArray[ i ].data[ j ].close = 1;
+                  dataArray[ i ].data[ j ].high = 1;
+                  dataArray[ i ].data[ j ].low = 1;
+                  dataArray[ i ].data[ j ].open = 1;
+                  dataArray[ i ].data[ j ].volumefrom = 1;
+                  dataArray[ i ].data[ j ].volumeto = 1;
+                }
+              }
+            }
+
+            app.addHtmlTable( dataArray );
+            app.saveFile( dataArray );
+          } );
+        }
+      } );
+    }
+  };
+
+
+  // TODO: more code readable
+  // on window ready set dates
+  var today = new Date();
+  beforeWeek = new Date();
+
+  beforeWeek.setDate( today.getDate() - 6 );
+
+  function formatDateForInput( date ) {
+    var years = date.getFullYear(),
+      months = date.getMonth() + 1,
+      days = date.getDate();
+
+    if ( months < 10 ) {
+      months = '0' + months;
+    }
+
+    if ( days < 10 ) {
+      days = '0' + days;
+    }
+
+    return years + '-' + months + '-' + days;
+  }
+
+  var _MS_PER_DAY = 1000 * 60 * 60 * 24;
+  // a and b are javascript Date objects
+  function dateDiffInDays( a, b ) {
+    // Discard the time and time-zone information.
+    var utc1 = Date.UTC( a.getFullYear(), a.getMonth(), a.getDate() );
+    var utc2 = Date.UTC( b.getFullYear(), b.getMonth(), b.getDate() );
+
+    return Math.floor( ( utc2 - utc1 ) / _MS_PER_DAY );
+  }
+
+  $( '#date-to' ).val( formatDateForInput( today ) );
+  $( '#date-from' ).val( formatDateForInput( beforeWeek ) );
+
+  app.days = dateDiffInDays( beforeWeek, today );
 
 
 
-	// search data
-	$('.js-search').click(function() {
-		if($('#date-to').val()) {
-			app.dateTo = Date.parse($('#date-to').val());
-		}
+  // search data
+  $( '.js-search' ).click( function() {
+    if ( $( '#date-to' ).val() ) {
+      app.dateTo = Date.parse( $( '#date-to' ).val() );
+    }
 
-		if($('#date-from').val()) {
-			app.dateFrom = Date.parse($('#date-from').val());
-		}
+    if ( $( '#date-from' ).val() ) {
+      app.dateFrom = Date.parse( $( '#date-from' ).val() );
+    }
 
-		if(Number($('#amount').val())) {
-			app.cryptocurrenciesAmount = Number($('#amount').val());
-		}
+    if ( Number( $( '#amount' ).val() ) ) {
+      app.cryptocurrenciesAmount = Number( $( '#amount' ).val() );
+    }
 
-		if($('#fiat').val().length > 0) {
-			app.fiat = $('#fiat').val()
-		}
+    if ( $( '#fiat' ).val().length > 0 ) {
+      app.fiat = $( '#fiat' ).val()
+    }
 
-		app.days = dateDiffInDays(new Date(app.dateFrom), new Date(app.dateTo));
+    app.days = dateDiffInDays( new Date( app.dateFrom ), new Date( app.dateTo ) );
 
-		console.log(app);
+    console.log( app );
 
-		app.init();
-	});
-	
-	// show settings
-	$('.js-show-settings').click(function() {
-		var $this = $(this),
-			$parent = $this.parents('.settings-wrap');
-		$parent.find('.settings').slideDown(400);
-		$this.hide();
-	});
-}());
+    app.init();
+  } );
+
+  // show settings
+  $( '.js-show-settings' ).click( function() {
+    var $this = $( this ),
+      $parent = $this.parents( '.settings-wrap' );
+    $parent.find( '.settings' ).slideDown( 400 );
+    $this.hide();
+  } );
+}() );
