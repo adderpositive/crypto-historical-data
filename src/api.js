fe95eeb8a90339d5498113ef55fdfbaf5bbdeb6b
Martin Homola <adder10@gmail.com>
2018-05-25 22:46:03 +0200

    remove jquery

--- src/api.js
+++ src/api.js
@@ -1,4 +1,3 @@
-import $ from 'jquery';
 import * as dom from './dom.js';
 
 function getSingleCryptoData( data ) {
@@ -79,6 +78,67 @@ function saveFile( data ) {
   });
 }
 
+function processData( currenciesData, generalData ) {
+  const requests = [];
+  const dataResponse = JSON.parse( currenciesData ).data;
+  const dataKeys = Object.keys( dataResponse );
+  const dataArray = [];
+  const {
+    amountOfCurrencies,
+    fiat,
+    days,
+    timestamp
+  } = generalData;
+  let i;
+
+  console.log( dataKeys );
+  // sorted keys
+  dataKeys.sort(( a, b ) => {
+    return dataResponse[ a ].rank - dataResponse[ b ].rank;
+  } );
+
+  for ( i in dataKeys ) {
+    // push to array item by rank
+    dataArray.push( dataResponse[ dataKeys[ i ] ] ); 
+
+    // create requests
+    requests.push( 
+      getSingleCryptoData({
+        fiat,
+        days,
+        symbol: dataArray[ i ].symbol,
+        timestamp
+      }) 
+    );
+  }
+
+  // aysnchronous operation - waiting for all requests will be done
+  Promise.all( requests ).then(( operations ) => {
+    let j;
+    const isBTCException = fiat === 'BTC' && dataArray[ i ].symbol === 'BTC';
+
+    for ( j in operations ) {
+      dataArray[ j ].data = JSON.parse(operations[j]).Data;
+
+      if ( isBTCException ) {
+        let h;
+
+        for ( h in dataArray[ i ].data ) {
+          dataArray[ j ].data[ h ].close = 1;
+          dataArray[ j ].data[ h ].high = 1;
+          dataArray[ j ].data[ h ].low = 1;
+          dataArray[ j ].data[ h ].open = 1;
+          dataArray[ j ].data[ h ].volumefrom = 1;
+          dataArray[ j ].data[ h ].volumeto = 1;
+        }
+      }
+    }
+
+    console.log( dataArray );
+    saveFile( dataArray );
+  });
+}
+
 /********************
  * 1. get all currencies from ajax call
  * 2. create request on all gotten currencies
@@ -87,74 +147,21 @@ function saveFile( data ) {
  *
  */
 export default function init( data ) {
-  let {
-    amountOfCurrencies,
-    fiat,
-    days,
-    timestamp
-  } = data;
-  
-  dom.addPreloader();
+  const request = new XMLHttpRequest();
 
-  $.ajax( {
-    url: './data-all-currencies',
-    method: 'POST',
-    dataType: 'json',
-    data: {
-      amountOfCurrencies
-    },
-    success: ( response ) => {
-      const requests = [];
-      const dataResponse = response.data;
-      const dataKeys = Object.keys( dataResponse );
-      const dataArray = [];
-      let i;
-
-      // sorted keys
-      dataKeys.sort(( a, b ) => {
-        return dataResponse[ a ].rank - dataResponse[ b ].rank;
-      } );
-
-      for ( i in dataKeys ) {
-        // push to array item by rank
-        dataArray.push( dataResponse[ dataKeys[ i ] ] ); 
-
-        // create requests
-        requests.push( 
-          getSingleCryptoData({
-            fiat,
-            days,
-            symbol: dataArray[ i ].symbol,
-            timestamp
-          }) 
-        ); 
+  request.open('POST', './data-all-currencies', true);
+  request.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+  request.onload = () => {
+    if ( request.readyState === request.DONE ) {
+      if ( request.status === 200 ) {
+        processData( request.response, data );
       }
-
-      // aysnchronous operation - waiting for all requests will be done
-      Promise.all( requests ).then(( operations ) => {
-        let j;
-        const isBTCException = fiat === 'BTC' && dataArray[ i ].symbol === 'BTC';
-
-        for ( j in operations ) {
-          dataArray[ j ].data = JSON.parse(operations[j]).Data;
-
-          if ( isBTCException ) {
-            let h;
-
-            for ( h in dataArray[ i ].data ) {
-              dataArray[ j ].data[ h ].close = 1;
-              dataArray[ j ].data[ h ].high = 1;
-              dataArray[ j ].data[ h ].low = 1;
-              dataArray[ j ].data[ h ].open = 1;
-              dataArray[ j ].data[ h ].volumefrom = 1;
-              dataArray[ j ].data[ h ].volumeto = 1;
-            }
-          }
-        }
-
-        console.log( dataArray );
-        saveFile( dataArray );
-      });
     }
-  });
+  };
+  dom.addPreloader();
+  request.send(
+    JSON.stringify({
+      amountOfCurrencies: data.amountOfCurrencies
+    })
+  );
 }
